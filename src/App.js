538a87947fa5179dedd8f6f9d097dbca231faf04
sachinjadhav <sachin.jadhav12@wipro.com>
2018-10-25 23:03:28 +0100

    heroku

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3002/imageurl', {
+      fetch('https://young-sierra-54682.herokuapp.com//imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3002/image', {
+          fetch('https://young-sierra-54682.herokuapp.com//image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3002/signin', {
+    fetch('https://young-sierra-54682.herokuapp.com//signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
